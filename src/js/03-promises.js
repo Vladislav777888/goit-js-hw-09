7ad2dfb6a77ed484678e42a6459ab489df619e33
Vladyslav Solomakha <vladislavsolomaha08@gmail.com>
2022-11-06 21:07:30 +0200

    solved task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const refs = {
+  form: document.querySelector('.form'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = evt.currentTarget;
+
+  let delayNumber = Number(delay.value);
+  let stepNumber = Number(step.value);
+  let amountNumber = Number(amount.value);
+
+  for (let i = 1; i <= amountNumber; i += 1) {
+    createPromise(i, delayNumber)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delayNumber += stepNumber;
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
